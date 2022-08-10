0f4f5cd9bc52957b6f58921610b9d5eb629597d4
Natalia Ratnikova <ratnikova.it@gmail.com>
2022-08-10 18:53:16 +0300

    task1 is finished

--- Tasks/NataliaRatnikova/task1.py
+++ Tasks/NataliaRatnikova/task1.py
@@ -1,13 +1,13 @@
 import decimal
-quantize(Decimal("1.00"))
-# decimal.getcontext().prec = 2
 
-deposit_summ = decimal.Decimal(input("Please enter the summ of your deposit:\n"))
+deposit_sum = decimal.Decimal(input("Please enter the sum of your deposit:\n"))
 deposit_term = decimal.Decimal(input("Please enter the term of your deposit(years):\n"))
 deposit_percent = decimal.Decimal(input("Please enter the percent of your deposit(for 1 year):\n"))
-capitalazation = str(input("Would you like to have capitalization each months?(enter yes or no):\n")) 
+capitalazation = str(input("Would you like to have monthly capitalization?(enter yes or no):\n")) 
 if capitalazation == "yes" or capitalazation == "Yes" or capitalazation == "Y" or capitalazation == "y":
-    print("The final summ of your deposit is", (deposit_summ*(1+deposit_percent/100/12)**(deposit_term*12)))   
+    final_sum = deposit_sum*(1+deposit_percent/100/12)**(deposit_term*12)
+    print("The final sum of your deposit is", final_sum.quantize(decimal.Decimal("1.00")))   
 else:
-    print("The final summ of your deposit is", (deposit_summ+deposit_summ*deposit_percent/100*deposit_term))
+    final_sum = deposit_sum+deposit_sum*deposit_percent/100*deposit_term
+    print("The final sum of your deposit is", final_sum)
 
