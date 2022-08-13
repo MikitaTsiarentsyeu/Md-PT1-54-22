5005555d3fd3abc8317f9780d799637e301b8a27
Reallocman <61756155+Reallocman@users.noreply.github.com>
2022-08-13 15:19:21 +0300

    decimal module applied
    applied decimal module instead of using float type,also added a check for the integer number of the entered years of the deposit

--- Tasks/Shkred/Task_1/task_1.py
+++ Tasks/Shkred/Task_1/task_1.py
@@ -1,30 +1,35 @@
-def validate_input(text, type):
+from decimal import Decimal 
+
+def validate_input(text):
     flag = False
     while not flag:
         inp = input(text +'\n')
+        if inp[0] == '-':
+            continue
         try: 
-            if type == 'int': 
-                result = int(inp)
-            elif type == 'float': 
-                result = float(inp) 
-            flag = True           
+            if text == "Enter the required period in years:":
+                if int(inp):
+                    pass
+            elif float(inp): 
+                pass       
+            flag = True               
         except:
-            flag = False
-    return result
+            pass
+    return inp
 
 while True:
-    initial_deposit_amount = validate_input("Enter the deposit amount:", "float") 
-    period = validate_input("Enter the required period in years:", "int") 
-    percentage = validate_input("Enter the annual percentage:", "float")/100  
-    monthly_capitalization = input("Enter Y to enable monthly capitalization:\n")
+    initial_deposit_amount = Decimal(validate_input("Enter the deposit amount:")) 
+    period = Decimal(validate_input("Enter the required period in years:"))
+    percentage = Decimal(validate_input("Enter the annual percentage:"))/100  
+    monthly_capitalization = input("Enter Y to enable monthly capitalization or press the enter button:\n")
 
-    if monthly_capitalization == 'Y':
+    if monthly_capitalization.upper() == 'Y':
         period *= 12
         percentage /= 12
     summ = initial_deposit_amount*((1+percentage)**period)
-    print('%.2f' %summ)
+    print(summ.quantize(Decimal('1.00')))
     
-    to_continue = input("Enter Y to start over:\n")
-    if to_continue != 'Y':
+    to_continue = input("Enter Y to start over or press the enter button:\n")
+    if to_continue.upper() != 'Y':
         break  
           
