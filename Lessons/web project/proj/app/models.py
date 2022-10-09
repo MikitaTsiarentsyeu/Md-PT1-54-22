f6ca2e37875fb40c3561beca9c056fe864dbb6a4
Mikita Tsiarentsyeu <Mikita_Tsiarentsyeu@epam.com>
2022-10-09 14:54:44 +0600

    add lesson 09/10

--- Lessons/web project/proj/app/models.py
+++ Lessons/web project/proj/app/models.py
@@ -1,3 +1,21 @@
+from random import choices
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+
+    name = models.CharField(max_length=200, blank=False)
+    email = models.EmailField(blank=False, primary_key=True)
+
+class Post(models.Model):
+
+    POST_TYPES = [('c', 'copyright'), ('p', 'public license')]
+
+    title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=300)
+    content = models.TextField()
+    issued = models.DateTimeField()
+    post_type = models.CharField(choices=POST_TYPES, max_length=1)
+    image = models.ImageField(upload_to='uploads')
+
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+
